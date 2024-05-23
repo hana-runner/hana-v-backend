507074f4b589ff4fbc09373466e4d7190371f1af
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-23 21:53:58 +0900

    chore: 프로젝트 생성 (#2)
    :tada: 프로젝트 생성
    ➕ spotless 적용
    ➕ lombok 적용

---
+++ src/test/java/com/v/hana/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.v.hana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
