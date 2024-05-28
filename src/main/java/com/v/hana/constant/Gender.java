0c161db0909f49b9cd32e5355634791ece845a6f
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-05-28 18:49:17 +0900

    ✅ Add UserJpaRepository method test

---
+++ src/main/java/com/v/hana/constant/Gender.java
@@ -0,0 +1,5 @@
+package com.v.hana.constant;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ src/main/java/com/v/hana/entity/User.java
@@ -0,0 +1,70 @@
+package com.v.hana.entity;
+
+import com.v.hana.constant.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+import java.time.LocalDate; // LocalDateTime 대신 LocalDate를 임포트합니다.
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //Entity e = new Entity()
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "pw", nullable = false)
+    private String pw;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "birthday", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday; // LocalDateTime 대신 LocalDate로 변경합니다.
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "gender", nullable = false, columnDefinition = "INT")
+    private Gender gender;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Timestamp updatedAt;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+    @Column(name = "is_receive_email")
+    private boolean isReceiveEmail = true;
+
+    @Column(name = "is_receive_alarm")
+    private boolean isReceiveAlarm = true;
+
+    @Builder
+    public User(String username, String pw, String email, String name, Gender gender, LocalDate birthday) { // LocalDateTime 대신 LocalDate로 변경합니다.
+        this.username = username;
+        this.pw = pw;
+        this.email = email;
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+}
