301c46ed36e3acf3035279e22b5e877c615c82ec
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-05-28 17:32:37 +0900

    🚧 Exclude DataSourceAutoConfig

--- src/main/java/com/v/hana/Application.java
+++ src/main/java/com/v/hana/Application.java
@@ -4,9 +4,10 @@ import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @TypeInfo(name = "Application", description = "어플리케이션 클래스")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     @MethodInfo(name = "main", description = "어플리케이션을 실행합니다.")
