56caa6460e8626201794cf16e142eae39b99fab6
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-30 15:43:30 +0900

    fix: Parameter 0 of constructor 오류 해결 (#29)
    :ambulance: 컴포넌트 스캔 수정
    :wrench: JPA 설정 추가
    :card_index: DB 계좌 칼럼 추가
    :card_index: DB에 코멘트 추가

--- src/main/java/com/v/hana/Application.java
+++ src/main/java/com/v/hana/Application.java
@@ -4,10 +4,9 @@ import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @TypeInfo(name = "Application", description = "어플리케이션 클래스")
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.v.hana")
 public class Application {
 
     @MethodInfo(name = "main", description = "어플리케이션을 실행합니다.")
---
+++ src/main/java/com/v/hana/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.v.hana.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.v.hana.repository")
+public class JpaConfig {}
