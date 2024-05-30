2a56c6f79f244e05cffc69f72fc976e60deb8595
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-30 12:11:37 +0900

    :zap: Valid 예외 처리 로직 리펙토링

--- src/main/java/com/v/hana/common/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/v/hana/common/exception/ExceptionControllerAdvice.java
@@ -3,8 +3,8 @@ package com.v.hana.common.exception;
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import com.v.hana.common.response.GlobalHttpCode;
-import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,18 +24,21 @@ class ExceptionControllerAdvice {
     }
 
     @MethodInfo(
-            name = "handleConstraintViolationException",
-            description = "서버에서 발생한 제약 조건 위반 예외를 처리합니다.")
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<BaseExceptionResponse> handleConstraintViolationException(
-            ConstraintViolationException e) {
+            name = "handleMethodArgumentNotValidException",
+            description = "서버에서 발생한 MethodArgumentNotValidException 예외를 처리합니다.")
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseExceptionResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException methodArgumentNotValidException) {
         return ResponseEntity.status(GlobalHttpCode.BAD_REQUEST.getHttpStatus())
                 .body(
                         BaseExceptionResponse.of(
                                 GlobalHttpCode.BAD_REQUEST.getHttpStatus(),
                                 GlobalHttpCode.BAD_REQUEST.getCode(),
-                                e.getMessage(),
-                                e.getClass().getSimpleName()));
+                                methodArgumentNotValidException
+                                        .getBindingResult()
+                                        .getFieldError()
+                                        .getDefaultMessage(),
+                                methodArgumentNotValidException.getClass().getSimpleName()));
     }
 
     @MethodInfo(
