7e9b30cebaa40b097440baa6152961de4d7c46c8
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-31 05:40:40 +0900

    :ambulance: single field constructor 오류 해결

--- src/main/java/com/v/hana/dto/transaction/TransactionHistoryPutRequest.java
+++ src/main/java/com/v/hana/dto/transaction/TransactionHistoryPutRequest.java
@@ -1,6 +1,7 @@
 package com.v.hana.dto.transaction;
 
 import jakarta.validation.constraints.NotNull;
+import java.beans.ConstructorProperties;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ public class TransactionHistoryPutRequest {
     @NotNull(message = "categoryId는 필수 입력 값입니다.")
     private final Long categoryId;
 
+    @ConstructorProperties({"categoryId"})
     @Builder
     public TransactionHistoryPutRequest(Long categoryId) {
         this.categoryId = categoryId;
