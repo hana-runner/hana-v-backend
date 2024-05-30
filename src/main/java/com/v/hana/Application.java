f803fcf389bdc22a2d9aa7b29e9950917cf01def
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-05-30 15:54:43 +0900

    🐛 Add import statment & apply spotless

--- src/main/java/com/v/hana/Application.java
+++ src/main/java/com/v/hana/Application.java
@@ -3,9 +3,12 @@ package com.v.hana;
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import java.util.Locale;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @TypeInfo(name = "Application", description = "어플리케이션 클래스")
 @SpringBootApplication(scanBasePackages = "com.v.hana")
--- src/test/java/com/v/hana/controller/user/UserControllerTest.java
+++ src/test/java/com/v/hana/controller/user/UserControllerTest.java
@@ -1,18 +1,15 @@
 package com.v.hana.controller.user;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
-import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.snippet.Attributes.key;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
-import com.epages.restdocs.apispec.SimpleType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
@@ -24,7 +21,6 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @TypeInfo(name = "ExampleControllerTest", description = "예시 컨트롤러 테스트 클래스")
 public class UserControllerTest extends RestDocsTest {
@@ -67,21 +63,47 @@ public class UserControllerTest extends RestDocsTest {
                                         ResourceSnippetParameters.builder()
                                                 .tag("user")
                                                 .summary("유저 회원 가입 API")
-                                                .description("유저 회원 가입 API입니다. 요청 데이터를 받아 응답 데이터를 반환합니다. 0을 넣으면 Exception이 발생합니다.")
+                                                .description(
+                                                        "유저 회원 가입 API입니다. 요청 데이터를 받아 응답 데이터를 반환합니다. 0을 넣으면 Exception이 발생합니다.")
                                                 .requestFields(
-                                                        fieldWithPath("username").description("사용자 이름").type(JsonFieldType.STRING),
-                                                        fieldWithPath("pw").description("비밀번호").type(JsonFieldType.STRING),
-                                                        fieldWithPath("name").description("이름").type(JsonFieldType.STRING),
-                                                        fieldWithPath("email").description("이메일").type(JsonFieldType.STRING),
-                                                        fieldWithPath("gender").description("성별").type(JsonFieldType.NUMBER))
+                                                        fieldWithPath("username")
+                                                                .description("사용자 이름")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("pw")
+                                                                .description("비밀번호")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("name")
+                                                                .description("이름")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("email")
+                                                                .description("이메일")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("gender")
+                                                                .description("성별")
+                                                                .type(JsonFieldType.NUMBER))
                                                 .responseFields(
-                                                        fieldWithPath("status").description("상태 코드").type(JsonFieldType.NUMBER),
-                                                        fieldWithPath("success").description("성공 여부").type(JsonFieldType.BOOLEAN),
-                                                        fieldWithPath("timestamp").description("응답 시간").type(JsonFieldType.STRING)
-                                                                .attributes(key("format").value("yyyy-MM-dd HH:mm:ss")),
-                                                        fieldWithPath("code").description("응답 코드").type(JsonFieldType.STRING),
-                                                        fieldWithPath("message").description("응답 메시지").type(JsonFieldType.STRING),
-                                                        fieldWithPath("response").description("응답 데이터").type(JsonFieldType.NUMBER))
+                                                        fieldWithPath("status")
+                                                                .description("상태 코드")
+                                                                .type(JsonFieldType.NUMBER),
+                                                        fieldWithPath("success")
+                                                                .description("성공 여부")
+                                                                .type(JsonFieldType.BOOLEAN),
+                                                        fieldWithPath("timestamp")
+                                                                .description("응답 시간")
+                                                                .type(JsonFieldType.STRING)
+                                                                .attributes(
+                                                                        key("format")
+                                                                                .value(
+                                                                                        "yyyy-MM-dd HH:mm:ss")),
+                                                        fieldWithPath("code")
+                                                                .description("응답 코드")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("message")
+                                                                .description("응답 메시지")
+                                                                .type(JsonFieldType.STRING),
+                                                        fieldWithPath("response")
+                                                                .description("응답 데이터")
+                                                                .type(JsonFieldType.NUMBER))
                                                 .build())));
     }
 }
