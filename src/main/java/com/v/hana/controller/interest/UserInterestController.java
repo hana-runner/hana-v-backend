9bb672a95495f73da4dfc2d349cf895d4742c75a
pocj8ur4in <pocj8ur4in@gmail.com>
2024-05-31 11:19:22 +0900

    :recycle: 사용자 관심사 목록 조회 API 리펙토링

--- src/main/java/com/v/hana/controller/interest/UserInterestController.java
+++ src/main/java/com/v/hana/controller/interest/UserInterestController.java
@@ -1,9 +1,10 @@
-package com.v.hana.controller;
+package com.v.hana.controller.interest;
 
+import com.v.hana.command.interest.GetUserInterestsCommand;
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import com.v.hana.dto.UserInterestResponse;
-import com.v.hana.service.interest.UserInterestService;
+import com.v.hana.usecase.interest.UserInterestUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("v1/api")
 public class UserInterestController {
-    private final UserInterestService userInterestService;
+    private final UserInterestUseCase userInterestUseCase;
 
     @MethodInfo(name = "getUserInterests", description = "사용자 관심사 목록 가져오기")
     @GetMapping("/user-interests/{userId}")
     public ResponseEntity<UserInterestResponse> getUserInterests(@PathVariable Long userId) {
-        UserInterestResponse interests = userInterestService.getUserInterests(userId);
+        UserInterestResponse interests =
+                userInterestUseCase.getUserInterests(
+                        GetUserInterestsCommand.builder().userId(userId).build());
         return ResponseEntity.ok(interests);
     }
 
-    public UserInterestController(UserInterestService userInterestService) {
-        this.userInterestService = userInterestService;
+    public UserInterestController(UserInterestUseCase userInterestUseCase) {
+        this.userInterestUseCase = userInterestUseCase;
     }
 }
--- src/main/java/com/v/hana/service/interest/UserInterestService.java
+++ src/main/java/com/v/hana/service/interest/UserInterestService.java
@@ -1,5 +1,7 @@
 package com.v.hana.service.interest;
 
+import com.v.hana.command.interest.GetUserInterestsCommand;
+import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
 import com.v.hana.dto.UserInterestDto;
 import com.v.hana.dto.UserInterestResponse;
@@ -7,33 +9,27 @@ import com.v.hana.entity.interest.Interest;
 import com.v.hana.entity.interest.UserInterest;
 import com.v.hana.repository.interest.InterestRepository;
 import com.v.hana.repository.interest.UserInterestRepository;
-import com.v.hana.repository.user.UserRepository;
+import com.v.hana.usecase.interest.UserInterestUseCase;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @TypeInfo(name = "UserInterestService", description = "예시 서비스 클래스")
 @Service
-public class UserInterestService {
+public class UserInterestService implements UserInterestUseCase {
+    private final InterestRepository interestRepository;
 
-    private final UserRepository userRepository;
+    private final UserInterestRepository userInterestRepository;
 
-    @Autowired
-    UserInterestService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Autowired private InterestRepository interestRepository;
-
-    @Autowired private UserInterestRepository userInterestRepository;
-
-    public UserInterestResponse getUserInterests(Long userId) {
-        ArrayList<UserInterest> userInterests = userInterestRepository.findByUserId(userId);
+    @MethodInfo(name = "getUserInterests", description = "유저의 관심사를 조회합니다.")
+    @Override
+    public UserInterestResponse getUserInterests(GetUserInterestsCommand command) {
+        ArrayList<UserInterest> userInterests =
+                userInterestRepository.findByUserId(command.getUserId());
 
         return UserInterestResponse.builder()
                 .data(
-                        userInterestRepository.findByUserId(userId).stream()
+                        userInterestRepository.findByUserId(command.getUserId()).stream()
                                 .map(
                                         userInterest -> {
                                             Interest interest =
@@ -52,5 +48,11 @@ public class UserInterestService {
                                         })
                                 .collect(Collectors.toCollection(ArrayList::new)))
                 .build();
+    }
+
+    public UserInterestService(
+            InterestRepository interestRepository, UserInterestRepository userInterestRepository) {
+        this.interestRepository = interestRepository;
+        this.userInterestRepository = userInterestRepository;
     }
 }
