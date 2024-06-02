d5d29374c447f50c73d693a0728ef056de240817
jyoon0531 <133069925+jyoon0531@users.noreply.github.com>
2024-06-02 10:57:57 +0900

    ✨ 사용자별 등록된 계좌 목록 조회 기능

--- src/main/java/com/v/hana/repository/account/AccountRepository.java
+++ src/main/java/com/v/hana/repository/account/AccountRepository.java
@@ -1,10 +1,15 @@
 package com.v.hana.repository.account;
 
 import com.v.hana.common.annotation.TypeInfo;
+import com.v.hana.dto.account.AccountDto;
 import com.v.hana.entity.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @TypeInfo(name = "AccountRepository", description = "계좌 레포지토리 인터페이스")
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {}
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    ArrayList<AccountDto> findByUserId(Long userId);
+}
