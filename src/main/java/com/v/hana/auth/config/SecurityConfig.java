cca8a8bdddd0efb4934f1b165a4865e723515e87
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-06-03 17:50:21 +0900

    🐛 Fix CORS ERROR

--- src/main/java/com/v/hana/auth/config/SecurityConfig.java
+++ src/main/java/com/v/hana/auth/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.v.hana.auth.config;
-
 import com.v.hana.auth.filter.JwtAuthenticationFilter;
 import com.v.hana.auth.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -11,40 +10,51 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
     private final JwtTokenProvider jwtTokenProvider;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable)
                 .sessionManagement(
-                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
-                        (authz) ->
-                                authz.requestMatchers(
-                                                "/swagger-ui/**",
-                                                "/v1/api/users/join",
-                                                "v1/api/users/login",
-                                                "v1/api/users/new_token",
-                                                "v1/api/users/check_dup",
-                                                "v1/api/users/update/pw",
-                                                "v1/api/users/find/username",
-                                                "v1/api/emails/authcode",
-                                                "v1/api/emails/check/authcode")
-                                        .permitAll()
-                                        .anyRequest()
-                                        .authenticated())
+                        authz -> authz.requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v1/api/users/join",
+                                        "v1/api/users/login",
+                                        "v1/api/users/new_token",
+                                        "v1/api/users/check_dup",
+                                        "v1/api/users/update/pw",
+                                        "v1/api/users/find/username",
+                                        "v1/api/emails/authcode",
+                                        "v1/api/emails/check/authcode")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173")); // 허용할 출처
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP method
+        config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type")); // 허용할 HTTP header
+        config.setAllowCredentials(true); // 쿠키 인증 요청 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
