6fe124809440e223ff043511e5afb53bdaf910d5
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-05 17:03:08 +0900

    ♻️ 거래내역 관련 API 응답 수정

--- src/main/java/com/v/hana/dto/transaction/TransactionHistoryDetailDto.java
+++ src/main/java/com/v/hana/dto/transaction/TransactionHistoryDetailDto.java
@@ -6,12 +6,15 @@ import lombok.Getter;
 
 @Getter
 public class TransactionHistoryDetailDto {
+    private final Long id;
     private final InterestDto interest;
     private final String description;
     private final Long amount;
 
     @Builder
-    public TransactionHistoryDetailDto(InterestDto interest, String description, Long amount) {
+    public TransactionHistoryDetailDto(
+            Long id, InterestDto interest, String description, Long amount) {
+        this.id = id;
         this.interest = interest;
         this.description = description;
         this.amount = amount;
