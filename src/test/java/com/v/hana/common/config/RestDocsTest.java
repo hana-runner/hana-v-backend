63d6218e12f4d9ce67fa706358c8fa9e56b4d6c6
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-05 14:10:10 +0900

    ✅ 테스트에 testcontainer 적용

---
+++ src/test/java/com/v/hana/common/config/RestDocsTest.java
@@ -0,0 +1,32 @@
+package com.v.hana.common.config;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.v.hana.common.annotation.MethodInfo;
+import com.v.hana.common.annotation.TypeInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@TypeInfo(name = "RestDocsTest", description = "Rest Docs 테스트 클래스")
+@ExtendWith({RestDocumentationExtension.class, MySQLTestConfig.class, RedisTestConfig.class})
+public abstract class RestDocsTest {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @MethodInfo(name = "setUp", description = "Rest Docs 설정을 초기화합니다.")
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentationContextProvider) {
+        this.mockMvc =
+                MockMvcBuilders.standaloneSetup(initializeController())
+                        .apply(documentationConfiguration(restDocumentationContextProvider))
+                        .build();
+    }
+
+    protected abstract Object initializeController();
+}
