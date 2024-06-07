6973483ab632fd2e3f5123b8999e14048a722b4b
jyoon0531 <133069925+jyoon0531@users.noreply.github.com>
2024-06-07 14:10:42 +0900

    refactor: 계좌 API 관련 응답 객체 수정 (#99)
    * ♻️ 계좌 유효성 검사 응답 객체 수정
    
    * ♻️ 계좌 등록 응답 객체 수정

--- src/main/java/com/v/hana/dto/account/AccountDto.java
+++ src/main/java/com/v/hana/dto/account/AccountDto.java
@@ -6,15 +6,16 @@ import lombok.Getter;
 @Getter
 public class AccountDto {
     private final Long id;
+    private final String bankName;
     private final String accountNumber;
     private final Long balance;
     private final String accountName;
     private final String accountType;
 
     @Builder
-    public AccountDto(
-            Long id, String accountNumber, Long balance, String accountName, String accountType) {
+    public AccountDto(Long id, String bankName, String accountNumber, Long balance, String accountName, String accountType) {
         this.id = id;
+        this.bankName = bankName;
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.accountName = accountName;
