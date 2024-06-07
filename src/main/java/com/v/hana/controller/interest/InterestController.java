b8b6306fb45f9a6ff96ed56a386dbab6dd18a63d
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-07 13:33:18 +0900

    refactor: 컨트롤러에 swagger 어노테이션 추가 (#97)
    ♻️ 컨트롤러에 swagger 어노테이션 추가

--- src/main/java/com/v/hana/controller/interest/InterestController.java
+++ src/main/java/com/v/hana/controller/interest/InterestController.java
@@ -2,8 +2,14 @@ package com.v.hana.controller.interest;
 
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
+import com.v.hana.common.exception.BaseExceptionResponse;
 import com.v.hana.dto.interest.InterestsResponse;
 import com.v.hana.usecase.interest.InterestUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 @TypeInfo(name = "InterestController", description = "관심사 컨트롤러")
 @RestController
+@Tag(name = "Interest", description = "관심사")
 @RequestMapping("v1/api")
 public class InterestController {
 
     private final InterestUseCase interestUseCase;
 
+    @Operation(
+            summary = "관심사 목록 가져오기",
+            description = "관심사 목록을 가져옵니다.",
+            method = "GET",
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "관심사 목록 가져오기 성공",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(implementation = InterestsResponse.class))),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "잘못된 요청",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "서버 에러",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class)))
+            })
     @MethodInfo(name = "getInterests", description = "관심사 목록 가져오기")
     @GetMapping("/interests")
     public ResponseEntity<InterestsResponse> getInterests() {
--- src/main/java/com/v/hana/controller/interest/UserInterestController.java
+++ src/main/java/com/v/hana/controller/interest/UserInterestController.java
@@ -7,22 +7,63 @@ import com.v.hana.command.interest.GetUserInterestTransactionsCommand;
 import com.v.hana.command.interest.GetUserInterestsCommand;
 import com.v.hana.common.annotation.MethodInfo;
 import com.v.hana.common.annotation.TypeInfo;
+import com.v.hana.common.exception.BaseExceptionResponse;
 import com.v.hana.dto.interest.UserInterestReportsResponse;
 import com.v.hana.dto.interest.UserInterestResponse;
 import com.v.hana.dto.interest.UserInterestTransactionsResponse;
 import com.v.hana.entity.user.User;
 import com.v.hana.usecase.interest.UserInterestUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @TypeInfo(name = "UserInterestController", description = "사용자 관심사 컨트롤러")
 @RestController
+@Tag(name = "UserInterest", description = "사용자 관심사")
 @RequestMapping("v1/api")
 public class UserInterestController {
     private final UserInterestUseCase userInterestUseCase;
     private final SecurityUtil securityUtil;
 
     @MethodInfo(name = "getUserInterests", description = "사용자 관심사 목록 가져오기")
+    @Operation(
+            summary = "사용자 관심사 목록 가져오기",
+            description = "사용자의 관심사 목록을 가져옵니다.",
+            parameters = {@Parameter(name = "userId", description = "사용자 ID", required = true)},
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "사용자 관심사 목록 가져오기 성공",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                UserInterestResponse.class))),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "사용자 관심사 목록 가져오기 실패",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "서버 에러",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class)))
+            })
     @GetMapping("/user-interests/{userId}")
     @CurrentUser
     public ResponseEntity<UserInterestResponse> getUserInterests(@PathVariable Long userId) {
@@ -35,6 +76,45 @@ public class UserInterestController {
     }
 
     @MethodInfo(name = "getUserInterests", description = "사용자 관심사별 거래 내역 가져오기")
+    @Operation(
+            summary = "사용자 관심사별 거래 내역 가져오기",
+            description = "사용자의 관심사별 거래 내역을 가져옵니다.",
+            parameters = {
+                @Parameter(name = "interestId", description = "관심사 ID", required = true),
+                @Parameter(name = "userId", description = "사용자 ID", required = true),
+                @Parameter(name = "year", description = "년도", required = true),
+                @Parameter(name = "month", description = "월", required = true)
+            },
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "사용자 관심사별 거래 내역 가져오기 성공",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                UserInterestTransactionsResponse
+                                                                        .class))),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "사용자 관심사별 거래 내역 가져오기 실패",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "서버 에러",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class)))
+            })
     @GetMapping("/user-interests/transaction/{interestId}")
     @CurrentUser
     public ResponseEntity<UserInterestTransactionsResponse> getUserInterestTransaction(
@@ -56,6 +136,45 @@ public class UserInterestController {
     }
 
     @MethodInfo(name = "getUserInterestReports", description = "사용자 관심사별 거래 내역 리포트를 가져옵니다.")
+    @Operation(
+            summary = "사용자 관심사별 거래 내역 리포트 가져오기",
+            description = "사용자의 관심사별 거래 내역 리포트를 가져옵니다.",
+            parameters = {
+                @Parameter(name = "interestId", description = "관심사 ID", required = true),
+                @Parameter(name = "userId", description = "사용자 ID", required = true),
+                @Parameter(name = "year", description = "년도", required = true),
+                @Parameter(name = "month", description = "월", required = true)
+            },
+            responses = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "사용자 관심사별 거래 내역 리포트 가져오기 성공",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                UserInterestReportsResponse
+                                                                        .class))),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "사용자 관심사별 거래 내역 리포트 가져오기 실패",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class))),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "서버 에러",
+                        content =
+                                @Content(
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                BaseExceptionResponse.class)))
+            })
     @GetMapping("/user-interests/report/{interestId}")
     @CurrentUser
     public ResponseEntity<UserInterestReportsResponse> getUserInterestReports(
