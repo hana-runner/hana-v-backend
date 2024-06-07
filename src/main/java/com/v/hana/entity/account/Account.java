f46e836e313a76fb016c576a5a67571c0b8961e6
jyoon0531 <133069925+jyoon0531@users.noreply.github.com>
2024-06-07 17:07:01 +0900

    🐛 삭제한 계좌 재등록 로직 수정

--- src/main/java/com/v/hana/entity/account/Account.java
+++ src/main/java/com/v/hana/entity/account/Account.java
@@ -17,7 +17,6 @@ import org.hibernate.annotations.Where;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLDelete(sql = "UPDATE accounts SET is_deleted = true WHERE id = ?")
-@Where(clause = "is_deleted = false")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/v/hana/repository/account/AccountRepository.java
+++ src/main/java/com/v/hana/repository/account/AccountRepository.java
@@ -4,11 +4,23 @@ import com.v.hana.common.annotation.TypeInfo;
 import com.v.hana.dto.account.AccountDto;
 import com.v.hana.entity.account.Account;
 import java.util.ArrayList;
+import java.util.Optional;
+
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @TypeInfo(name = "AccountRepository", description = "계좌 레포지토리 인터페이스")
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     ArrayList<AccountDto> findByUserId(Long userId);
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+
+    @Query(value = "UPDATE accounts a SET a.is_deleted = false WHERE a.id = :accountId", nativeQuery = true)
+    @Modifying
+    @Transactional
+    void updateIsDeleted(Long accountId);
 }
