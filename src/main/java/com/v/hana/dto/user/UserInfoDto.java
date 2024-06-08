7ed9e3fbbd760d52063175d0ca896de77b55d995
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-06-08 16:20:55 +0900

    ♻️ Move UserInfo into data

---
+++ src/main/java/com/v/hana/dto/user/UserInfoDto.java
@@ -0,0 +1,14 @@
+package com.v.hana.dto.user;
+
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserInfoDto {
+    private String username;
+    private String email;
+    private LocalDate birthday;
+    private int gender;
+}
