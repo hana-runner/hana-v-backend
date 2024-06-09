6b6a7b5edd5f5cfc95862d4b7a1d0b1d70cc0546
pocj8ur4in <pocj8ur4in@gmail.com>
2024-06-09 14:25:40 +0900

    ♻️ API 로직 수정

--- src/main/java/com/v/hana/event/interest/SumAmountEventListener.java
+++ src/main/java/com/v/hana/event/interest/SumAmountEventListener.java
@@ -6,6 +6,7 @@ import com.v.hana.dto.interest.UserInterestReportsDto;
 import com.v.hana.dto.interest.UserInterestReportsResponse;
 import com.v.hana.usecase.transaction.TransactionHistoryDetailUseCase;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import lombok.Builder;
 import org.springframework.stereotype.Component;
@@ -21,10 +22,12 @@ public class SumAmountEventListener {
     public UserInterestReportsResponse handle(SumAmountEvent sumAmountEvent) {
         Long userId = sumAmountEvent.getUserId();
 
-        LocalDate end = LocalDate.of(sumAmountEvent.getYear(), sumAmountEvent.getMonth(), 1);
+        LocalDate end =
+                LocalDate.of(sumAmountEvent.getYear(), sumAmountEvent.getMonth(), 1)
+                        .with(TemporalAdjusters.lastDayOfMonth());
 
         if (end.isAfter(LocalDate.now())) {
-            end = LocalDate.now();
+            end = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
         }
 
         LocalDate start = end.minusMonths(6).withDayOfMonth(1);
@@ -32,19 +35,28 @@ public class SumAmountEventListener {
         Long myAverage =
                 transactionHistoryDetailUseCase.sumAmountByUserIdAndInterestId(
                         userId, sumAmountEvent.getInterestId(), start, end);
+
+        if (myAverage == null) {
+            myAverage = 0L;
+        }
+
         Long peerAverage =
                 transactionHistoryDetailUseCase.sumAmountByInterestId(
                         sumAmountEvent.getInterestId(), start, end);
 
+        if (peerAverage == null) {
+            peerAverage = 0L;
+        }
+
         ArrayList<Long> myMonth = new ArrayList<>();
 
-        for (int i = 1; i <= 6; i++) {
-            start = end.minusMonths(i).withDayOfMonth(1);
-            end = end.minusMonths(i - 1).withDayOfMonth(1);
+        for (int i = 0; i < 6; i++) {
+            LocalDate monthStart = end.minusMonths(i).withDayOfMonth(1);
+            LocalDate monthEnd = monthStart.with(TemporalAdjusters.lastDayOfMonth());
 
             Long sumAmount =
                     transactionHistoryDetailUseCase.sumAmountByUserIdAndInterestId(
-                            userId, sumAmountEvent.getInterestId(), start, end);
+                            userId, sumAmountEvent.getInterestId(), monthStart, monthEnd);
 
             if (sumAmount == null) {
                 sumAmount = 0L;
