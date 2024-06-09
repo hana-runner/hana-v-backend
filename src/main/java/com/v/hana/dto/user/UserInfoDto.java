4943568a8291d11baa8c5cfc0782f99711f290ef
campus2 H035 <h035@alpacoedu.onmicrosoft.com>
2024-06-09 17:28:10 +0900

    ♻️ Get users/info 응답 data에 name 추가

--- src/main/java/com/v/hana/dto/user/UserInfoDto.java
+++ src/main/java/com/v/hana/dto/user/UserInfoDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class UserInfoDto {
+    private String name;
     private String username;
     private String email;
     private LocalDate birthday;
